11ff9a9e99ba8512a1fa4cba03b2628d5064631b
hgrandine <hugog_3@hotmail.com>
2020-02-29 10:30:04 -0300

    Primeira classe testada, categoria

--- src/main/java/com/mecontrola/spring/resources/CategoriaResource.java
+++ src/main/java/com/mecontrola/spring/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.mecontrola.spring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mecontrola.spring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "ok";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Andaime");
+		Categoria cat2 = new Categoria(2, "Rodinha");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 }

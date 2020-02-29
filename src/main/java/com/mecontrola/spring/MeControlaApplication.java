eecab99699bffa5282633c6dc896e6ffc6538fc3
hgrandine <hugog_3@hotmail.com>
2020-02-29 09:31:20 -0300

    Projeto criado

---
+++ src/main/java/com/mecontrola/spring/MeControlaApplication.java
@@ -0,0 +1,13 @@
+package com.mecontrola.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeControlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeControlaApplication.class, args);
+	}
+
+}

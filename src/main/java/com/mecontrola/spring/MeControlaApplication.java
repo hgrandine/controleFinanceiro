4d735e90efcb3b8f3a525d6037249f1cd69de8b2
hgrandine <hugog_3@hotmail.com>
2020-04-12 11:48:50 -0300

    criando operacao de intanciacao

--- src/main/java/com/mecontrola/spring/MeControlaApplication.java
+++ src/main/java/com/mecontrola/spring/MeControlaApplication.java
@@ -1,13 +1,31 @@
 package com.mecontrola.spring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MeControlaApplication {
+import com.mecontrola.spring.domain.Categoria;
+import com.mecontrola.spring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class MeControlaApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRep;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MeControlaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		categoriaRep.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }

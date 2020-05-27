9355874dae4e8ed91419807719c5b39993930a3f
hgrandine <hugog_3@hotmail.com>
2020-05-27 13:27:45 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/mecontrola/spring/services/CategoriaService.java
+++ src/main/java/com/mecontrola/spring/services/CategoriaService.java
@@ -1,12 +1,12 @@
 package com.mecontrola.spring.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mecontrola.spring.domain.Categoria;
 import com.mecontrola.spring.repositories.CategoriaRepository;
+import com.mecontrola.spring.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -14,7 +14,8 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Optional<Categoria> buscar(Integer id) {
-		return repo.findById(id);		
-	}
+	 public Categoria buscar(Integer id) {
+		 return repo.findById(id).orElseThrow(() -> new
+				 ObjectNotFoundException( "Objeto nao encontrado! id: " + id + ", Tipo: " + Categoria.class.getName())); }
+	 
 }

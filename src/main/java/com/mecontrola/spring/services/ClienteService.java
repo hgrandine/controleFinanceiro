95ae8f418fdaa386d30cb6ed10765e013c4e14f5
hgrandine <hugog_3@hotmail.com>
2020-07-18 15:17:08 -0300

    end point cliente

---
+++ src/main/java/com/mecontrola/spring/services/ClienteService.java
@@ -0,0 +1,21 @@
+package com.mecontrola.spring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mecontrola.spring.domain.Cliente;
+import com.mecontrola.spring.repositories.ClienteRepository;
+import com.mecontrola.spring.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	 public Cliente buscar(Integer id) {
+		 return repo.findById(id).orElseThrow(() -> new
+				 ObjectNotFoundException( "Objeto nao encontrado! id: " + id + ", Tipo: " + Cliente.class.getName())); }
+	 
+}

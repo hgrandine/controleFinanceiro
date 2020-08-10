4e92bc93b9e8e426c7229becf83baa3a7b34b6ea
hgrandine <hugog_3@hotmail.com>
2020-08-10 15:02:15 -0300

    Delete categoria

--- src/main/java/com/mecontrola/spring/services/CategoriaService.java
+++ src/main/java/com/mecontrola/spring/services/CategoriaService.java
@@ -2,9 +2,11 @@ package com.mecontrola.spring.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import com.mecontrola.spring.domain.Categoria;
 import com.mecontrola.spring.repositories.CategoriaRepository;
+import com.mecontrola.spring.services.exceptions.DataIntegrityException;
 import com.mecontrola.spring.services.exceptions.ObjectNotFoundException;
 
 
@@ -26,6 +28,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 	 
 }

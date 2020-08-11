f21b744994e7f7bb364b2f3531d51ac4f6313e83
hgrandine <hugog_3@hotmail.com>
2020-08-11 18:25:41 -0300

    paginacao com parametros opcionais

--- src/main/java/com/mecontrola/spring/MeControlaApplication.java
+++ src/main/java/com/mecontrola/spring/MeControlaApplication.java
@@ -70,6 +70,11 @@ public class MeControlaApplication implements CommandLineRunner{
 
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "cama");
+		Categoria cat4 = new Categoria(null, "mesa");
+		Categoria cat5 = new Categoria(null, "eletro");
+		Categoria cat6 = new Categoria(null, "decoracao");
+		Categoria cat7 = new Categoria(null, "perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -82,7 +87,7 @@ public class MeControlaApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRep.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRep.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRep.saveAll(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/mecontrola/spring/services/CategoriaService.java
+++ src/main/java/com/mecontrola/spring/services/CategoriaService.java
@@ -3,8 +3,11 @@ package com.mecontrola.spring.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
 
 import com.mecontrola.spring.domain.Categoria;
 import com.mecontrola.spring.repositories.CategoriaRepository;
@@ -44,6 +47,9 @@ public class CategoriaService {
 		}
 	}
 
-
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
 	 
 }

77df88e5904820928389ad121f4c435d92122270
hgrandine <hugog_3@hotmail.com>
2020-08-13 14:59:34 -0300

    Inserir novo Cliente com enderecos (POST)

--- src/main/java/com/mecontrola/spring/resources/ClienteResource.java
+++ src/main/java/com/mecontrola/spring/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.mecontrola.spring.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mecontrola.spring.domain.Cliente;
 import com.mecontrola.spring.dto.ClienteDTO;
+import com.mecontrola.spring.dto.ClienteNewDTO;
 import com.mecontrola.spring.services.ClienteService;
 
 @RestController
@@ -66,5 +69,15 @@ public class ClienteResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO){
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		//pega a uri que da solicitacao
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/mecontrola/spring/services/ClienteService.java
+++ src/main/java/com/mecontrola/spring/services/ClienteService.java
@@ -8,10 +8,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mecontrola.spring.domain.Cidade;
 import com.mecontrola.spring.domain.Cliente;
+import com.mecontrola.spring.domain.Endereco;
+import com.mecontrola.spring.domain.enums.TipoCliente;
 import com.mecontrola.spring.dto.ClienteDTO;
+import com.mecontrola.spring.dto.ClienteNewDTO;
 import com.mecontrola.spring.repositories.ClienteRepository;
+import com.mecontrola.spring.repositories.EnderecoRepository;
 import com.mecontrola.spring.services.exceptions.DataIntegrityException;
 import com.mecontrola.spring.services.exceptions.ObjectNotFoundException;
 
@@ -20,30 +26,36 @@ import com.mecontrola.spring.services.exceptions.ObjectNotFoundException;
 public class ClienteService {
 
 	@Autowired
-	private ClienteRepository repo;
+	private ClienteRepository repoCli;
+	
+	@Autowired
+	private EnderecoRepository repoEnd;
 	
 	 public Cliente find(Integer id) {
-		 return repo.findById(id).orElseThrow(() -> new
+		 return repoCli.findById(id).orElseThrow(() -> new
 				 ObjectNotFoundException( "Objeto nao encontrado! id: " + id + ", Tipo: " + Cliente.class.getName())); }
 	 
 	 public List<Cliente> findAll() {
-			return repo.findAll();
+			return repoCli.findAll();
 		}
 		 
+	 	@Transactional
 		public Cliente insert(Cliente obj) {
 			obj.setId(null);
-			return repo.save(obj);
+			obj = repoCli.save(obj);
+			repoEnd.saveAll(obj.getEnderecos());
+			return obj;
 		}
 
 		public Cliente update(Cliente obj) {
 			Cliente newObj = find(obj.getId());
 			updateData(newObj, obj);
-			return repo.save(newObj);
+			return repoCli.save(newObj);
 		}
 
 		public void delete(Integer id) {
 			try {
-				repo.deleteById(id);
+				repoCli.deleteById(id);
 			}catch (DataIntegrityViolationException e) {
 				throw new DataIntegrityException("Não é possivel excluir porque ha entidades relacionadas");
 			}
@@ -51,12 +63,29 @@ public class ClienteService {
 
 		public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-			return repo.findAll(pageRequest);
+			return repoCli.findAll(pageRequest);
 		}
 		
 		//instancia uma categoria a partir de um dto
 		public Cliente fromDTO(ClienteDTO objDTO) {
 			return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+		}
+		
+		public Cliente fromDTO(ClienteNewDTO objDTO) {
+			Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), 
+					TipoCliente.toEnum(objDTO.getTipo()));
+			
+			Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+			
+			Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), 
+					objDTO.getBairro(), objDTO.getCep(), cli, cid);
+			
+			cli.getEnderecos().add(end);
+			cli.getTelefones().add(objDTO.getTelefone1());
+			if(objDTO.getTelefone2() != null) cli.getTelefones().add(objDTO.getTelefone2());
+			if(objDTO.getTelefone3() != null) cli.getTelefones().add(objDTO.getTelefone3());
+			
+			return cli;
 		}
 		 
 		private void updateData(Cliente newObj, Cliente obj) {

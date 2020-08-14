28314582fc74ad17fec08f4ad5047227a3e04b5b
hgrandine <hugog_3@hotmail.com>
2020-08-14 13:27:47 -0300

    Deletando enderecos junto com o cliente

--- src/main/java/com/mecontrola/spring/services/ClienteService.java
+++ src/main/java/com/mecontrola/spring/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 			try {
 				repoCli.deleteById(id);
 			}catch (DataIntegrityViolationException e) {
-				throw new DataIntegrityException("Não é possivel excluir porque ha entidades relacionadas");
+				throw new DataIntegrityException("Não é possivel excluir porque ha pedidos relacionadas");
 			}
 		}
 
